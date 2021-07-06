652dfeaf698930e5cddc1608a8fc48ad3fb2848d
VGol-Git <61864261+VGol-Git@users.noreply.github.com>
2021-07-06 19:16:33 +0300

    add lab1

---
+++ lab1/lab1/Exam.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Uni
+{
+    public class Exam
+    {
+
+        public string Subject { get; set; }
+        public int Grade { get; set; }
+        public DateTime DateExam { get; set; }
+
+        public Exam(String subject, int assessment, DateTime dateExam)
+        {
+            this.Subject = subject;
+            this.Grade = assessment;
+            this.DateExam = dateExam;
+
+
+        }
+
+
+        public Exam()
+        {
+            this.Subject = "NoSubject";
+            this.Grade = 0;
+            this.DateExam.AddDays(0);
+            this.DateExam.AddMonths(0);
+            this.DateExam.AddYears(0);
+        }
+
+        public override string ToString() => Subject + ' ' + Grade + ' ' + DateExam;
+
+    }
+}
